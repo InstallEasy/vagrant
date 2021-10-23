cf3d95f792d43530082898d51ba28951bf980ea3
krlex <krle@tilda.center>
2021-10-23 21:42:03 +0200

    update for installation

--- install.sh
+++ install.sh
@@ -1,45 +1,55 @@
 #!/usr/bin/env bash
 
-export UBUNTU=""
-export DEBIAN=""
-export FEDORA=""
-export CENTOS=""
+export UBUNTU="vagrant"
+export DEBIAN="vagrant"
+export FEDORA="vagrant"
+export CENTOS="vagrant"
 
-  install_debian() {
+install_debian() {
+    $SUDO apt install -y curl
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | $SUDO apt-key add -
+    $SUDO apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
     $SUDO apt update
     $SUDO apt install -y $DEBIAN
   
     echo " Packages is ready for Debian"
     echo " Finished Packages installation"
   
-  }
+}
 
-  install_ubuntu() {
+install_ubuntu() {
+    $SUDO apt install -y curl
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | $SUDO apt-key add -
+    $SUDO apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
     $SUDO apt update
     $SUDO apt install -y $UBUNTU 
   
     echo " Packages is ready for Ubuntu"
     echo " Finished Packages installation"
   
-  }
+}
   
-  install_fedora() {
+install_fedora() {
+    $SUDO dnf install -y dnf-plugins-core
+    $SUDO config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
     $SUDO dnf update -y
     $SUDO dnf -y install $FEDORA 
   
     echo " Packages is ready for Fedora"
     echo " Finished Packages installation"
   
-  }
+}
 
-  install_centos() {
+install_centos() {
+    $SUDO yum install -y yum-utils
+    $SUDO yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
     $SUDO dnf update -y
     $SUDO dnf -y install $CENTOS
   
     echo " Packages is ready for CentOS"
     echo " Finished Packages installation"
   
-  }
+}
   
   usage() {
     echo
